342b2724ca8d1d46388f916298441d6ac0fbaddd
kenway <>
2025-06-05 18:55:20 +0300

    kenway
    added main file and sup files

---
+++ Cafe/main.cpp
@@ -0,0 +1,10 @@
+﻿// main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "CafeSimulator.h"
+
+int main() {
+    CafeSimulator cafe;
+    cafe.run();
+    return 0;
+}
